35e51a2b7f5f5d6661f8b1bb917409404db711f1
Z640 <53868989+Z640@users.noreply.github.com>
2020-08-19 01:45:07 +0900

    update

--- New Project/Assets/Scripts/Player/PlayerController.cs
+++ New Project/Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float walkSpeed;
     [SerializeField] private float jumpForce;
     private Vector3 velocity; //움직이는 속도
+    private float move_direction = 0f;
     private bool isGround;
 
     private BoxCollider2D myCollider;
@@ -24,28 +25,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isGround)
+        {
+            Movement();
+            Jump();
+            //Move();
+        }
         IsGround();
-        Jump();
-        Move();
     }
-    
+
+    private void Movement()
+    {
+        move_direction = Mathf.Lerp(move_direction, Input.GetAxisRaw("Horizontal"), 0.1f);
+        if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            move_direction = Mathf.Lerp(move_direction, 0, 0.5f); ;
+        }
+        velocity = (transform.right * move_direction).normalized * walkSpeed * 0.1f;
+        myRigid.MovePosition(transform.position + velocity);
+    }
+
     private void Move()
     {
         float move_direction = Input.GetAxisRaw("Horizontal");
-        velocity = (transform.right * move_direction).normalized * walkSpeed;
+        velocity = (transform.right * move_direction).normalized * walkSpeed * 0.1f;
         myRigid.MovePosition(transform.position + velocity);
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        //점프 거리 조절
+        if (Input.GetButtonDown("Jump")) //점프 시작
         {
-            myRigid.velocity = transform.up * jumpForce/* + velocity * jumpForce*/;
+            myRigid.velocity = transform.up * jumpForce + velocity * jumpForce * 5;
+        }
+        else if (Input.GetButtonUp("Jump")) //점프 끝
+        {
+
         }
     }
-    
+
     private void IsGround()
     {
-        isGround = Physics2D.Raycast(transform.position, Vector3.down, myCollider.bounds.extents.y + 0.05f);
+        isGround = Physics2D.Raycast(transform.position, Vector3.down, myCollider.bounds.extents.y + 0.1f);
     }
 }
