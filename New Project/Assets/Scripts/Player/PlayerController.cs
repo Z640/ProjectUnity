2f858d140757c64fe88d73ae76176dd1309da02d
Z640 <53868989+Z640@users.noreply.github.com>
2020-08-25 22:46:20 +0900

    총추가, IK추가, 총 조작(움직임) 추가
    애니메이션 고장 (IK로 바꾸기)

--- New Project/Assets/Scripts/Player/PlayerController.cs
+++ New Project/Assets/Scripts/Player/PlayerController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     // 움직임 관련 변수들
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
@@ -16,87 +15,85 @@ public class PlayerController : MonoBehaviour
     private bool isRun;
 
     [SerializeField] private Camera cam;
+    [SerializeField] private GameObject holder;
+    [SerializeField] private GameObject gunHolder;
     private BoxCollider2D myCollider;
     private Rigidbody2D myRigid;
     private Animator anime;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         myRigid = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<BoxCollider2D>();
         anime = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(curSpeed);
+    void Update() {
         Move();
         Run();
         CamMovement();
-        if (isGround)
-        {
+        if (isGround) {
             //Movement();
             Jump();
             //Move();
         }
         IsGround();
         Animation();
+        HolderRotation();
     }
 
-    private void CamMovement()
-    {
+    private void HolderRotation() {
+        Vector3 lookat = Camera.main.ScreenToWorldPoint(Input.mousePosition) - holder.transform.position;
+        float AngleRad = Mathf.Atan2(lookat.y, lookat.x);
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        holder.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+    }
+
+    private void CamMovement() {
         cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(this.transform.position.x, 1f, -1f), 0.2f);
     }
 
-    private void Animation()
-    {
+    private void Animation() {
         anime.SetBool("isWalk", isWalk);
         anime.SetBool("isRun", isRun && isWalk);
     }
 
-    private void Move()
-    {
+    private void Move() {
         float move_direction = Input.GetAxisRaw("Horizontal");
         if (move_direction > 0) {
-            this.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
-        }else if (move_direction < 0) {
+            this.transform.localEulerAngles = Vector3.zero;
+            gunHolder.transform.localEulerAngles = Vector3.zero;
+        } else if (move_direction < 0) {
             this.transform.localEulerAngles = new Vector3(0f, 180f, 0f);
+            gunHolder.transform.localEulerAngles = new Vector3(180f, 0f, 0f);
         }
         isWalk = (move_direction != 0);
         velocity = (transform.right * Mathf.Abs(move_direction)).normalized * curSpeed * 0.1f;
         myRigid.MovePosition(transform.position + velocity);
     }
 
-    private void Run()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
+    private void Run() {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
             curSpeed = runSpeed;
             isRun = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
+        } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
             curSpeed = walkSpeed;
             isRun = false;
         }
     }
 
-    private void Jump()
-    {
+    private void Jump() {
         //점프 거리 조절
         if (Input.GetButtonDown("Jump")) //점프 시작 
         {
-            myRigid.velocity = transform.up * jumpForce;
-        }
-        else if (Input.GetButtonUp("Jump")) //점프 끝
-        {
-        }
+            myRigid.AddForce(transform.up * jumpForce);
+            //myRigid.velocity = transform.up * jumpForce;
+        } else if (Input.GetButtonUp("Jump")) //점프 끝
+        { }
     }
 
-    private void IsGround()
-    {
-        isGround = Physics2D .Raycast(transform.position, Vector3.down, myCollider.bounds.extents.y + 0.1f);
+    private void IsGround() {
+        isGround = Physics2D.Raycast(transform.position, Vector3.down, myCollider.bounds.extents.y + 0.1f);
     }
 }
