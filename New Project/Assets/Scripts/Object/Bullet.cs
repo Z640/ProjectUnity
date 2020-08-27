a08629793f27e2f26ebe31c3ee0592c5b22f8dd6
Z640 <53868989+Z640@users.noreply.github.com>
2020-08-27 16:23:41 +0900

    이펙트 추가

--- New Project/Assets/Scripts/Object/Bullet.cs
+++ New Project/Assets/Scripts/Object/Bullet.cs
@@ -2,20 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
-    private Gun currentGun;
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if(other != null)
-        {
-            //if (other.transform.tag == "NPC")
-                //other.transform.GetComponent<Animal>().Damage(currentGun.damage, transform.position);
+public class Bullet : MonoBehaviour {
+    public GameObject hitEF;
+    private void OnCollisionEnter2D(Collision2D col) {
+        if (col != null) {
+            if (col.transform.tag != "Player") {
+                Destroy(Instantiate(hitEF, transform.position, Quaternion.identity), 1f);
+                Destroy(this.gameObject);
+            }
         }
-        Destroy (this.gameObject);
     }
-
     void Update() {
         transform.right = GetComponent<Rigidbody2D>().velocity;
     }
