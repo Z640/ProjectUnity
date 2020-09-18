56b31d4328d432d3e069bb5b50ba67395bd85b42
TYon327 <taeyounggreat@gmail.com>
2020-09-18 16:06:22 +0900

    프로그래밍 할 프로젝트
    실제 프로그래밍들은 이쪽에다가 해주세요

---
+++ Unity Programming/Assets/Scripts/Entity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+
+    private bool dead;
+    private bool visible;
+    private float health;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    bool is_dead()
+    {
+        return dead;
+    }
+    bool is_visible()
+    {
+        return visible;
+    }
+    float get_health()
+    {
+        return health;
+    }
+}
